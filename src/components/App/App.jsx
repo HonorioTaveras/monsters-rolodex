8a3c7baa59d510badeae2c6cd4957575a532689c
Honorio <htaveras90@gmail.com>
2020-09-28 19:37:00 -0600

    implemented SearchBox component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,11 +4,13 @@ import Axios from 'axios';
 
 // COMPONENTS
 import { CardList } from '../CardList/CardList.jsx';
+import { SearchBox } from '../SearchBox/SearchBox.jsx';
 
 export default function App() {
   const [err, setErr] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState('');
 
   useEffect(() => {
     Axios.get('https://jsonplaceholder.typicode.com/users').then(
@@ -23,9 +25,21 @@ export default function App() {
     );
   }, []);
 
+  const handleChange = (e) => {
+    setSearchField(e.target.value);
+  }
+
+  const filteredMonsters = monsters.filter((monster) =>
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+  );
+
   return (
     <div className='App'>
-      <CardList monsters={monsters} />
+      <SearchBox
+        placeholder="search monsters"
+        handleChange={handleChange}
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
---
+++ src/components/dummData.js
@@ -0,0 +1,16 @@
+const monsters = [
+  {
+    name: 'Frankenstein',
+    id: 1,
+  },
+  {
+    name: 'Dracula',
+    id: 2,
+  },
+  {
+    name: 'Zombie',
+    id: 3,
+  },
+];
+
+export default monsters;
